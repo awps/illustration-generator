94183974b5cf1d79eee16d12e33e028fa0a1af40
awps <asurdu@awesomemotive.com>
2026-03-12 17:42:48 +0200

    chore: reconfigure project for Cloudflare Workers with R2 and Wrangler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+import { Env } from "./types";
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response("illustrations-generator is running", {
+      headers: { "Content-Type": "text/plain" },
+    });
+  },
+} satisfies ExportedHandler<Env>;
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface Env {
+  IMAGES_BUCKET: R2Bucket;
+  GEMINI_API_KEY: string;
+  CLOUDFLARE_ACCOUNT_ID: string;
+  AI_GATEWAY_ID: string;
+  IMAGES_DOMAIN: string;
+}
